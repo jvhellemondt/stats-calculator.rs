53446dac5895397c5d70c637b37cadeaa4659772
Jens van Hellemondt <jens@invictus.codes>
2024-10-13 12:46:35 +0200

    feat(stats-calculator): it_should_calculate_that_the_minimum_value_in_the_range_3_2_4_5_is_2

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,27 @@ fn main() {
 }
 
 #[derive(Debug, PartialEq)]
+enum StatsCalculatorErrors {
+    NoValues
+}
+
+#[derive(Debug, PartialEq)]
 struct StatsCalculator;
 
 impl StatsCalculator {
-    fn calculate(_input: Vec<i32>) -> i32 {
-        1
+    fn calculate(input: Vec<i32>) -> Result<i32, StatsCalculatorErrors> {
+        if input.is_empty() {
+            return Err(StatsCalculatorErrors::NoValues);
+        }
+
+        let mut min_value: i32 = input[0];
+
+        for &i in &input {
+            if i < min_value {
+                min_value = i;
+            }
+        }
+        Ok(min_value)
     }
 }
 
@@ -26,6 +42,13 @@ mod tests {
     fn it_should_calculate_that_the_minimum_value_in_the_range_1_2_3_4_5_is_1() {
         let input = vec![1, 2, 3, 4, 5];
         let result = StatsCalculator::calculate(input);
-        assert_eq!(result, 1);
+        assert_eq!(result, Ok(1));
+    }
+
+    #[rstest]
+    fn it_should_calculate_that_the_minimum_value_in_the_range_3_2_4_5_is_2() {
+        let input = vec![3, 2, 4, 5];
+        let result = StatsCalculator::calculate(input);
+        assert_eq!(result, Ok(2));
     }
 }
