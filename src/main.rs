94f4279b8459d261256cc84fa596d44f0f78a553
Jens van Hellemondt <jens@invictus.codes>
2024-10-13 14:00:10 +0200

    feat(stats-calculator): calculate_amount_of_elements

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ struct StatsCalculator;
 struct StatsSummary {
     min_value: i32,
     max_value: i32,
+    amount_of_elements: usize
 }
 
 impl StatsCalculator {
@@ -26,9 +27,9 @@ impl StatsCalculator {
         }
         min_value
     }
-    fn get_max_value(input: Vec<i32>) -> i32 {
+    fn get_max_value(input: &Vec<i32>) -> i32 {
         let mut max_value: i32 = input[0];
-        for i in input {
+        for &i in input {
             if i > max_value {
                 max_value = i;
             }
@@ -36,13 +37,19 @@ impl StatsCalculator {
         max_value
     }
 
+    fn calculate_amount_of_elements(input: Vec<i32>) -> usize {
+        input.len()
+    }
+
     pub fn summarize(input: Vec<i32>) -> Result<StatsSummary, StatsCalculatorErrors> {
         if input.is_empty() { return Err(StatsCalculatorErrors::NoValues); };
         let min_value = StatsCalculator::get_min_value(&input);
-        let max_value = StatsCalculator::get_max_value(input);
+        let max_value = StatsCalculator::get_max_value(&input);
+        let amount_of_elements = StatsCalculator::calculate_amount_of_elements(input);
         Ok(StatsSummary {
             min_value,
-            max_value
+            max_value,
+            amount_of_elements
         })
     }
 }
@@ -65,8 +72,8 @@ mod tests {
     }
 
     #[rstest]
-    #[case(vec![1, 2, 3, 4, 5], StatsSummary {min_value: 1,max_value: 5 })]
-    #[case(vec![3, 2, 4, 5], StatsSummary {min_value: 2,max_value: 5 })]
+    #[case(vec![1, 2, 3, 4, 5], StatsSummary {min_value: 1,max_value: 5, amount_of_elements: 5 })]
+    #[case(vec![3, 2, 4, 5], StatsSummary {min_value: 2,max_value: 5, amount_of_elements: 4 })]
     fn it_should_calculate_the_minimum_value(#[case] input: Vec<i32>, #[case] expected: StatsSummary) {
         let result = StatsCalculator::summarize(input);
         assert_eq!(result, Ok(expected));
