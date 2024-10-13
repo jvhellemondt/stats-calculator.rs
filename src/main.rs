582478835ea93bb160a47c07719f61417dadf2de
Jens van Hellemondt <jens@invictus.codes>
2024-10-13 12:30:42 +0200

    feat(stats-calculator): it_should_be_defined

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[derive(Debug, PartialEq)]
+struct StatsCalculator;
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+    use super::*;
+
+    #[rstest]
+    fn it_should_be_defined() {
+        let calculator = StatsCalculator;
+        assert_eq!(calculator, StatsCalculator);
+    }
+}
