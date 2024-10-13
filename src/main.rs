1af6f99cd8c2fe0fe9fa2a185d399ebdb8a51481
Jens van Hellemondt <jens@invictus.codes>
2024-10-13 12:36:23 +0200

    feat(stats-calculator): it_should_calculate_that_the_minimum_value_in_the_range_1_2_3_4_5_is_1

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ fn main() {
 #[derive(Debug, PartialEq)]
 struct StatsCalculator;
 
+impl StatsCalculator {
+    fn calculate(_input: Vec<i32>) -> i32 {
+        1
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::rstest;
@@ -14,5 +20,12 @@ mod tests {
     fn it_should_be_defined() {
         let calculator = StatsCalculator;
         assert_eq!(calculator, StatsCalculator);
+    }
+
+    #[rstest]
+    fn it_should_calculate_that_the_minimum_value_in_the_range_1_2_3_4_5_is_1() {
+        let input = vec![1, 2, 3, 4, 5];
+        let result = StatsCalculator::calculate(input);
+        assert_eq!(result, 1);
     }
 }
